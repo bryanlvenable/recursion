f02c49ef3eb46f05841f6cd816533562d0b713ca
Bryan L. Venable <bryanlvenable@gmail.com>
2014-11-29 13:31:24 -0800

    Removed unnecessary comments

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,25 +4,6 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
-  /*
-  // Function to check for the easy stuff
-  var simpleString = function(checkee) {
-  	if (typeof obj === checkee) {
-  		return String(obj);
-  	}
-  };
-  // Array of simple objects
-  simpleObjs = ['number'];
-  for (var i=0; i<simpleObjs.length; i++) {
-  	return simpleString(simpleObjs[i]);
-  }
-  // Check for reserved words
-  var reservedWords = [null];
-  var reservedStrings = ['null'];
-  if (obj === reservedWords[0]) {
-  	return reservedStrings[0];
-  }
-  */
   if (typeof obj === 'number') {
   	return String(obj);
   }
